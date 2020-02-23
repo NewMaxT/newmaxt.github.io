a0c7b6e7d0e241b43e28aab40231cb033376d2ad
Max <zentax.esport@gmail.com>
2020-02-24 00:03:27 +0100

    Initial commit

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - MILSIM</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+</head>
+
+<body>
+
+
+    <header>
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li><a href="../index.php">Accueil</a></li>
+                        <li><a href="../boutique.php">Boutique</a></li>
+                        <li><a class="active" href="../contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        <h1 class="h1_centered">OPERATION BLOODEAGLE</h1>
+    </header>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - MILSIM</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+</head>
+
+<body>
+
+
+    <header>
+        <a href="#" class="site-logo">Opérations BloodEagles</a>
+            <div>
+                <nav class="main-head">
+                    <ul class="head__list">
+                        <li><a class="head__link active" href="../index.php">Accueil</a></li>
+                        <li><a class="head__link" href="../boutique.php">Boutique</a></li>
+                        <li><a class="head__link" href="../contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        <nav class="Compte_Gestion">
+            <ul class="head__list">
+                <li class="head__list-item">
+                    <a class="head__link ul--btn" href="#">Se connecter</a>
+                 </li>
+                <li class="head__list-item">
+                    <a class="head__link ul--btn ul--btn--highlight" href="#">S'enregistrer</a>
+                 </li>
+            </ul>
+      </nav>
+    </header>
+
+    <main>
+
+    <section class="header_img">
+        <h1>SERVEUR DE SIMULATION MILITAIRE</h1>
+        <img src="../Images/logo.jpg">
+        </section>
+
+      <div class="space"></div>
+
+      <div class="home-about">
+        <h2>Notre organisation</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        <div class="columns">
+          <div>
+            <h3>Armée de l'air</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+              maiores fuga eos provident voluptas perferendis.
+            </p>
+          </div>
+          <div>
+            <h3>Armée de Terre</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus minima quo beatae eius blanditiis officiis.
+            </p>
+          </div>
+          <div>
+            <h3>Système hiérarchique repensé</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+              quasi quis doloribus quia illum laudantium.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="space"></div>
+
+</body>
+</html>
